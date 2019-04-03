113947feb580cbb10abacd73c8ef52336f229c44
Vim <vim@noodle.io>
2019-04-04 11:57:03 +1300

    Import and render component from Admin package

--- app/javascript/packs/admin.js
+++ app/javascript/packs/admin.js
@@ -1,17 +1,11 @@
-/* eslint no-console:0 */
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-//
-// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
-// layout file, like app/views/layouts/application.html.erb
-
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from 'frontend/src/Header';
 
 console.log('Hello World from admin webpacker');
+
+const LocalHeader = () => <h1>This is from the local webpacker</h1>;
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(<Header />, document.getElementById('app'));
+});
