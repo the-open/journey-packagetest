77f64be2733b81a99d8ab6223d629101fb79c47f
Vim <vim@noodle.io>
2019-04-04 13:58:24 +1300

    Test loading of other assets within admin bundle

--- journey_packagetest_admin/app/frontend/src/AdminHeader.js
+++ journey_packagetest_admin/app/frontend/src/AdminHeader.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import CoreHeader from 'journey-core-frontend/src/CoreHeader';
+import './index.css';
+import logo from './journey-logo.png';
 
 export default () => (
   <div>
     <h1>This is from Admin</h1>
+    <img alt="Journey Logo" src={logo} />
     This contains a reference to a component from the Core:
     <CoreHeader />
   </div>
